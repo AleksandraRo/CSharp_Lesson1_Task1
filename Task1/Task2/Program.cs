de1ccc06e2377ee0470a581e6f18a160c1d59b3d
AleksandraRo <79119954295@yandex.ru>
2023-12-27 21:59:40 +0300

    добавлены операвции с переменными. Task2

---
+++ Task1/Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Diagnostics.CodeAnalysis;
+
+int i1 = 5;
+int i2 = 6;
+int Sum = i1 + i2;
+Console.WriteLine(Sum);
+
+long l1 = 6;
+long Sum2 = l1 + i1;
+Console.WriteLine(Sum2);
+
+double d1 = 5.5;
+double Sum3 = d1 + i1;
+Console.WriteLine(Sum3);
+
+int product1 = i1 * i2;
+long product2 = l1 * i1;
+double product3 = d1 * i1;
+Console.WriteLine(product1);
+Console.WriteLine(product2);
+Console.WriteLine(product3);
